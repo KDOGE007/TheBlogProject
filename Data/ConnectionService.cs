b7ee683bb84408b4602a60bc7e467621d3c2524f
Kharn Tupmongkol <kharnthorn@gmail.com>
2022-04-03 22:53:24 +0100

    add the database service to use Heroku's Postgres

---
+++ Data/ConnectionService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Npgsql;
+using Microsoft.Extensions.Configuration;
+
+namespace TheBlogProject.Data
+{
+    public class ConnectionService
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConneciton");
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionService(databaseUrl);
+
+        }
+        private static string BuildConnectionService(string databaseUrl)
+        {
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+
+            return new NpgsqlConnectionStringBuilder()
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Prefer,
+                TrustServerCertificate = true
+            }.ToString();
+        }
+    }
+}
